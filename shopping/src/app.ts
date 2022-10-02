04d52243735320cdb82aeba2e39c7be59a4fb41a
Md. Tuhin Saud <tuhinsaud4614@gmail.com>
2022-10-03 01:55:44 +0600

    Customer server setup

---
+++ shopping/src/app.ts
@@ -0,0 +1,19 @@
+import compression from 'compression';
+import cors from 'cors';
+import type { Application } from 'express';
+import express, { json, urlencoded } from 'express';
+import helmet from 'helmet';
+
+const app: Application = express();
+
+app.use(helmet());
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use(compression());
+app.use(cors());
+
+app.use('/', (_, res) => {
+  res.json('hello');
+});
+
+export default app;
---
+++ shopping/src/utils/constants.ts
@@ -0,0 +1,4 @@
+/* eslint-disable import/prefer-default-export */
+
+/* A TypeScript feature that allows you to create a tuple of strings. */
+export const SIGNALS = ['SIGINT', 'SIGTERM', 'SIGHUP'] as const;
